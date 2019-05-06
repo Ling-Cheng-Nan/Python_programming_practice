1b27d08d4403152c2ec37e85ed6eba35faa4c638
Ling-Cheng-Nan <nan800130@gmail.com>
2019-05-06 16:07:32 +0800

    Question15.py be modiified, define a function to alternative sequentiaL code.

--- Question15.py
+++ Question15.py
@@ -9,15 +9,15 @@ Then, the output should be:
 Hints:
 In case of input data being supplied to the question, it should be assumed to be a console input.
 '''
-'''
-def question15add(n):
-	n1 = int("%s%s%s%s", (n,n,n,n))
-	n2 = int("%s%s%s", (n,n,n))
-	n3 = int("%s%s", (n,n))
-	n4 = int("%s", (n))
 
-	return n1+n2+n3+n4
-'''
+def question15add(n):
+	a1 = int("%s%s%s%s" %(n,n,n,n))
+	a2 = int("%s%s%s" %(n,n,n))
+	a3 = int("%s%s" %(n,n))
+	a4 = int("%s" %n)
+
+	print(a1+a2+a3+a4)
+
 a = input("Enter a given digit as the value of a:\n")
 
 n1 = int( "%s" % a )
@@ -26,5 +26,4 @@ n3 = int( "%s%s%s" % (a,a,a) )
 n4 = int( "%s%s%s%s" % (a,a,a,a) )
 
 print (n1+n2+n3+n4)
-
-#question15add(inp)
+question15add(a)
